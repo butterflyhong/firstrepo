adfaefd8b76b7a6be94f03c1085aa9d1551deb1b
Zhang Hong <honzhang@linkedin.com>
2015-03-28 18:11:17 +0800

    test jenkins output

--- httpd.go
+++ httpd.go
@@ -50,7 +50,7 @@ func main() {
 		c.String(http.StatusOK, message)
 	})
 
-	r.GET("/user/:name/*action", func(c *gin.Context) {
+	s.GET("/user/:name/*action", func(c *gin.Context) {
 		name := c.Params.ByName("name")
 		action := c.Params.ByName("action")
 		message := name + " is " + action
